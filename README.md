6b18a55d9523c3d51e2cbc04e81b9da610ad093a
rasang <1285847284@qq.com>
2020-04-24 17:35:08 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# same-IP-website-query-tool
+
+本菜鸡写了一个利用站长工具来查询同IP网站的工具，用起来比较方便，通过简单的正则提取文本信息
+
+# Usage:
+python 同IP网站查询.py [url]
---
+++ 同IP网站查询.py
@@ -0,0 +1,32 @@
+import requests
+import re
+from sys import argv
+
+try:
+    urlToQuery = argv[1]
+except IndexError:
+    print("请指定要查询的域名，格式如：\npython 同IP网站查询.py taobao.com")
+    exit(1)
+
+pageNumber = 1
+finalResult = []
+regRule = '<a href=\'javascript:\' onclick="window.open\(\'http.*?\'\)'
+
+try:
+    html = requests.get("http://stool.chinaz.com/same?s=" + urlToQuery + "&page=" + str(pageNumber)).text
+    pageResult = re.findall(regRule, html)
+
+    while len(pageResult) != 0:
+        finalResult += pageResult
+        pageNumber += 1
+        html = requests.get("http://stool.chinaz.com/same?s=" + urlToQuery + "&page=" + str(pageNumber)).text
+        pageResult = re.findall(regRule, html)
+
+except requests.exceptions.ConnectionError:
+    print("请检查你的网络连接")
+    exit(1)
+
+print("共查询到" + str(finalResult.__len__()) + "条结果")
+
+for i in finalResult:
+    print(i[44:-2])
